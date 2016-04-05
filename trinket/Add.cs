6e271a167fe7bdb6973702601bfd0e137ec1ad84
Claude Henchoz <claude.henchoz@gmail.com>
2016-04-05 18:53:36 +0200

    Close on Cancel and ESC

--- trinket/Add.cs
+++ trinket/Add.cs
@@ -17,6 +17,20 @@ namespace trinket
             InitializeComponent();
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
 
     }
 }
